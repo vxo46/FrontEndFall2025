01a38d89599c81709b867c2d2322a2d6fee3742c
vxo46 <vxo46@miami.edu>
2025-10-16 11:31:43 -0400

    Update script.js

--- guesstheword/script.js
+++ guesstheword/script.js
@@ -1,1 +1,11 @@
+"use script";
+let textInput = document.getElementById("mytext");
+let button = document.GetElementById("MyButton");
+let output = document.GetElementsByClassName("output");
+let incorrect = document.GetElementById("incorrect");
 
+let letters = ['i','b','i','s'];
+console.log(letters[1]);
+console.log(letter.length);
+
+button.addElementListener
