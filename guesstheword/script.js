98de123adaf14fc16af226d8b3adbace56ecc83c
vxo46 <vxo46@miami.edu>
2025-10-16 11:50:00 -0400

    Update script.js

--- guesstheword/script.js
+++ guesstheword/script.js
@@ -8,4 +8,12 @@ let letters = ['i','b','i','s'];
 console.log(letters[1]);
 console.log(letter.length);
 
-button.addElementListener
+button.addElementListener("click", function(){
+  console.log("textinput: "+ textInput.value);
+
+  for(let i = 0; i < letters.length; i++){
+    if(textInput.value == letter[i]{
+      output[i].innerHTML = letters[i];
+    }
+  }
+});
